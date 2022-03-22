92ea3be7be41e16105e2a1ec658157da12be4c2e
Devansh3712 <devanshamity@gmail.com>
2022-03-22 18:38:44 +0530

    Add `InvalidFingerprintError` to `__init__.py`

--- torweather/__init__.py
+++ torweather/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from torweather.email import Email
 from torweather.exceptions import EmailSendError
+from torweather.exceptions import InvalidFingerprintError
 from torweather.exceptions import ServiceBuildError
 from torweather.relay import Relay
 from torweather.schemas import Notif
