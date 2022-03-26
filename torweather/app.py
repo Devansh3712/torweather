75a2c57c676e303369b764ff5d325cc3c1924f52
Devansh3712 <devanshamity@gmail.com>
2022-03-26 17:47:18 +0530

    Create error pages, separate flask routes

--- torweather/app.py
+++ torweather/app.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 import os
-from collections.abc import Sequence
 
 from flask import Flask
 from flask import render_template
 from flask import request
 
 from torweather.check import Check
-from torweather.exceptions import InvalidEmailError
-from torweather.exceptions import InvalidFingerprintError
-from torweather.exceptions import RelayNotSubscribedError
-from torweather.relay import Relay
-from torweather.schemas import Notif
+from torweather.routes.subscribe import subscribe
+from torweather.routes.unsubscribe import unsubscribe
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -24,96 +20,17 @@ def home():
     return render_template("about.html")
 
 
-@app.route("/subscribe", methods=["GET", "POST"])
-def subscribe():
-    if request.method == "POST":
-        email: str = request.form.get("email")
-        fingerprint: str = request.form.get("fingerprint")
-        node_down: str = request.form.get("node-down")  # outputs "on" if checked
-        duration: int = (
-            int(request.form.get("duration"))
-            if request.form.get("duration") != ""
-            else 48
-        )
-        duration_type: str = request.form.get("duration-type")
-        notifs: Sequence[Notif] = []
-        if node_down == "on":
-            notifs.append(Notif.NODE_DOWN)
-        if duration_type == "days":
-            duration *= 24
-        elif duration_type == "weeks":
-            duration *= 24 * 7
-        elif duration_type == "months":
-            duration *= 24 * 7 * 30
-        try:
-            relay = Relay(fingerprint, testing=True)
-            result = relay.subscribe(email, notifs, duration)
-            if result:
-                return render_template(
-                    "subscribe.html",
-                    subscribed=True,
-                    nickname=relay.data.nickname,
-                    fingerprint=fingerprint,
-                )
-            else:
-                return render_template(
-                    "subscribe.html",
-                    subscribed=False,
-                    nickname=relay.data.nickname,
-                    fingerprint=fingerprint,
-                )
-        except InvalidEmailError:
-            return render_template("subscribe.html", error="Not a valid email address.")
-        except InvalidFingerprintError:
-            return render_template(
-                "subscribe.html", error="Not a valid relay fingerprint."
-            )
-    return render_template("subscribe.html")
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
-@app.route("/unsubscribe", methods=["GET", "POST"])
-def unsubscribe():
-    if request.method == "POST":
-        fingerprint: str = request.form.get("fingerprint")
-        email: str = request.form.get("email")
-        try:
-            relay = Relay(fingerprint, testing=True)
-            if email != relay.email:
-                return render_template(
-                    "unsubscribe.html", error="Email not subscribed by relay."
-                )
-            result = relay.unsubscribe()
-            if result:
-                return render_template(
-                    "unsubscribe.html",
-                    unsubscribed=True,
-                    nickname=relay.data.nickname,
-                    fingerprint=fingerprint,
-                )
-            else:
-                return render_template(
-                    "unsubscribe.html",
-                    unsubscribed=False,
-                    nickname=relay.data.nickname,
-                    fingerprint=fingerprint,
-                )
-        except InvalidEmailError:
-            return render_template(
-                "unsubscribe.html", error="Not a valid email address."
-            )
-        except InvalidFingerprintError:
-            return render_template(
-                "unsubscribe.html", error="Not a valid relay fingerprint."
-            )
-        except RelayNotSubscribedError:
-            return render_template(
-                "unsubscribe.html",
-                unsubscribed=False,
-                nickname=relay.data.nickname,
-                fingerprint=fingerprint,
-            )
-    return render_template("unsubscribe.html")
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 
 if __name__ == "__main__":
-    app.run()
+    app.register_blueprint(subscribe, url_prefix="/subscribe")
+    app.register_blueprint(unsubscribe, url_prefix="/unsubscribe")
+    app.run(debug=True)
---
+++ torweather/routes/subscribe.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+from collections.abc import Sequence
+
+from flask import Blueprint
+from flask import render_template
+from flask import request
+
+from torweather.exceptions import InvalidEmailError
+from torweather.exceptions import InvalidFingerprintError
+from torweather.relay import Relay
+from torweather.schemas import Notif
+
+subscribe = Blueprint("subscribe", __name__)
+
+
+@subscribe.route("/", methods=["GET", "POST"], strict_slashes=False)
+def main():
+    if request.method == "POST":
+        email: str = request.form.get("email")
+        fingerprint: str = request.form.get("fingerprint")
+        node_down: str = request.form.get("node-down")  # outputs "on" if checked
+        duration: int = (
+            int(request.form.get("duration"))
+            if request.form.get("duration") != ""
+            else 48
+        )
+        duration_type: str = request.form.get("duration-type")
+        notifs: Sequence[Notif] = []
+        if node_down == "on":
+            notifs.append(Notif.NODE_DOWN)
+        if duration_type == "days":
+            duration *= 24
+        elif duration_type == "weeks":
+            duration *= 7 * 24
+        elif duration_type == "months":
+            duration *= 30 * 24
+        try:
+            if not notifs:
+                return render_template(
+                    "subscribe.html",
+                    error="Choose at least one notification to subscribe.",
+                )
+            relay = Relay(fingerprint, testing=True)
+            result = relay.subscribe(email, notifs, duration)
+            if result:
+                return render_template(
+                    "subscribe.html",
+                    subscribed=True,
+                    nickname=relay.data.nickname,
+                    fingerprint=fingerprint,
+                )
+            else:
+                return render_template(
+                    "subscribe.html",
+                    subscribed=False,
+                    nickname=relay.data.nickname,
+                    fingerprint=fingerprint,
+                )
+        except InvalidEmailError:
+            return render_template("subscribe.html", error="Not a valid email address.")
+        except InvalidFingerprintError:
+            return render_template(
+                "subscribe.html", error="Not a valid relay fingerprint."
+            )
+    return render_template("subscribe.html")
