f1d6b9257c81546a9e8471a51f46af4245d87999
Devansh3712 <devanshamity@gmail.com>
2022-03-27 11:50:06 +0530

    Update `schemas.py`, `utils.py`

--- torweather/schemas.py
+++ torweather/schemas.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python
 import enum
+import os
 from collections.abc import Mapping
 from collections.abc import Sequence
 from datetime import datetime
 
 from pydantic import BaseModel
 
-from torweather.utils import email_headers
+
+def email_content(subject: str, file_name: str) -> Mapping[str, str]:
+    """Create a dictionary of subject and message of email.
+
+    Args:
+        subject (str): Subject of the email.
+        file_name (str): Name of file with email body.
+
+    Returns:
+        Mapping[str, str]: Email content.
+    """
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(current_directory, "messages", file_name)) as file:
+        content: Mapping[str, str] = {
+            "subject": f"[Tor Weather] {subject}",
+            "message": file.read(),
+        }
+    return content
 
 
 class Notif(enum.Enum):
     """Enum for types of notifications for Tor weather."""
 
-    NODE_DOWN: Mapping[str, str] = email_headers("Node down", "node_down.txt")
+    NODE_DOWN: Mapping[str, str] = email_content("Node down", "node_down.txt")
     SECURITY_VULNERABILITY: Mapping[str, str]
     END_OF_LIFE_VER: Mapping[str, str]
-    OUTDATED_VER: Mapping[str, str] = email_headers(
+    OUTDATED_VER: Mapping[str, str] = email_content(
         "Node out of date", "outdated_version.txt"
     )
     DNS_FAILURE: Mapping[str, str]
--- torweather/utils.py
+++ torweather/utils.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import os
 from collections.abc import Mapping
 from datetime import datetime
 
@@ -7,7 +6,7 @@ from torweather.schemas import RelayData
 
 
 def node_down_duration(relay: RelayData) -> int:
-    """Returns the duration of a TOR relay being down in hours.
+    """Returns the duration of a Tor relay being down in hours.
 
     Args:
         relay (RelayData): Data of the relay to check.
@@ -19,13 +18,3 @@ def node_down_duration(relay: RelayData) -> int:
     duration = current_time - relay.last_seen
     hours = divmod(duration.total_seconds(), 3600)[0]
     return int(hours)
-
-
-def email_headers(subject: str, file_name: str) -> Mapping[str, str]:
-    current_directory = os.path.dirname(os.path.realpath(__file__))
-    with open(os.path.join(current_directory, "messages", file_name)) as content:
-        headers: Mapping[str, str] = {
-            "subject": f"[Tor Weather] {subject}",
-            "message": content.read(),
-        }
-    return headers
